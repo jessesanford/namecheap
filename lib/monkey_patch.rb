13f626be321c6db19edea014a0bcbacb7bbb6b03
Jese Sanford <jesse@jessesanford.com>
2013-09-07 17:08:50 -0700

    turned off protected on to_query

--- lib/monkey_patch.rb
+++ lib/monkey_patch.rb
@@ -21,9 +21,6 @@ class Hash
     self
   end
 
-
-  protected
-
   def to_query(key)
     require 'cgi' unless defined?(CGI) && defined?(CGI::escape)
     "#{CGI.escape(key.to_param)}=#{CGI.escape(to_param.to_s)}"
